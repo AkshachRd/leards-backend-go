519d174363003fff900997654938d54f82a3ccfe
Daniil <dr.chashka@gmail.com>
2023-09-18 11:07:53 +0300

    Убрал баг с использованием добавления нового пользователя.

--- main.go
+++ main.go
@@ -19,26 +19,11 @@ func main() {
 	if err != nil {
 		return
 	}
-	user, err := models.NewUser("Admin", "admin@leards.space", "123")
+
+	err = models.NewUser(db, "Admin", "admin@leards.space", "123")
 	if err != nil {
 		return
 	}
-	// Create
-	db.Create(user)
-	//
-	//// Read
-	//var product Product
-	//db.First(&product, 1)                 // find product with integer primary key
-	//db.First(&product, "code = ?", "D42") // find product with code D42
-	//
-	//// Update - update product's price to 200
-	//db.Model(&product).Update("Price", 200)
-	//// Update - update multiple fields
-	//db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
-	//db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
-	//
-	//// Delete - delete product
-	//db.Delete(&product, 1)
 
 	r := gin.Default()
 
