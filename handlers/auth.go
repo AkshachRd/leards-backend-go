b93cfc5654d43c946995b438c2a3444af5c0699a
Daniil <dr.chashka@gmail.com>
2023-10-06 13:58:10 +0300

    Почнил эндпоинты для работы с токенами.

--- handlers/auth.go
+++ handlers/auth.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"github.com/AkshachRd/leards-backend-go/models"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"net/http"
 )
 
@@ -23,14 +22,8 @@ import (
 // @Router       /auth/{id} [get]
 func (s *Server) RefreshToken(c *gin.Context) {
 	id := c.Param("id")
-	userUuid, err := uuid.Parse(id)
 
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "User id parsing error"})
-		return
-	}
-
-	user, err := models.FetchUserById(s.db, userUuid)
+	user, err := models.FetchUserById(s.db, id)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
 		return
@@ -70,20 +63,13 @@ func (s *Server) RefreshToken(c *gin.Context) {
 // @Security     Bearer
 // @Param		 id	  path		string	true	"User ID"
 // @Success      200  {object}  httputils.BasicResponse
-// @Failure      400  {object}  httputils.HTTPError
 // @Failure      401  {object}  httputils.HTTPError
 // @Failure      500  {object}  httputils.HTTPError
 // @Router       /auth/{id} [delete]
 func (s *Server) RevokeToken(c *gin.Context) {
 	id := c.Param("id")
-	userUuid, err := uuid.Parse(id)
 
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "User id parsing error"})
-		return
-	}
-
-	user, err := models.FetchUserById(s.db, userUuid)
+	user, err := models.FetchUserById(s.db, id)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
 		return
