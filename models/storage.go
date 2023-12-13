cc9a644871bb29318beb889f444a46f876f9b865
Daniil <dr.chashka@gmail.com>
2023-12-13 11:46:35 +0300

    Переписал запрос для поиска по названию.

--- models/storage.go
+++ models/storage.go
@@ -15,28 +15,24 @@ type SearchResult struct {
 func SearchByNameWithPagination(name string, sortType string, orderBy string, page int, pageSize int) (*[]SearchResult, error) {
 	var results []SearchResult
 
-	folderQuery := db.Table("folder").
-		Select("folder.id_folder AS id, folder.name AS name, COUNT(favorite_storage.storage_id) AS rating, ? AS type, user.profile_icon_path", StorageTypeFolder).
-		Joins("LEFT JOIN favorite_storage ON favorite_storage.storage_id = folder.id_folder AND favorite_storage.storage_type = type").
-		Joins("LEFT JOIN permission ON permission.storage_id = folder.id_folder AND permission.storage_type = type").
-		Joins("LEFT JOIN user ON permission.user_id = user.id_user").
-		Where("folder.name LIKE ? AND folder.access_type = ?", fmt.Sprintf("%%%s%%", name), AccessTypePrivate).
-		Group("folder.id_folder")
-	// TODO: Change to AccessTypePublic
-	deckQuery := db.Table("deck").
-		Select("deck.id_deck AS id, deck.name AS name, COUNT(favorite_storage.storage_id) AS rating, ? AS type, user.profile_icon_path", StorageTypeDeck).
-		Joins("LEFT JOIN favorite_storage ON favorite_storage.storage_id = deck.id_deck AND favorite_storage.storage_type = type").
-		Joins("LEFT JOIN permission ON permission.storage_id = deck.id_deck AND permission.storage_type = type").
-		Joins("LEFT JOIN user ON permission.user_id = user.id_user").
-		Where("deck.name LIKE ? AND deck.access_type = ?", fmt.Sprintf("%%%s%%", name), AccessTypePrivate).
-		Group("deck.id_deck")
+	combinedQuery := db.Raw("? UNION ALL ?",
+		db.Table("folder").
+			Select("folder.id_folder AS id, folder.name, folder.access_type, ? AS type", StorageTypeFolder),
+		db.Table("deck").
+			Select("deck.id_deck AS id, deck.name, deck.access_type, ? AS type", StorageTypeDeck),
+	)
 
-	offset := (page - 1) * pageSize
-
-	err := db.
-		Raw(fmt.Sprintf("SELECT * FROM (?) UNION ALL SELECT * FROM (?) ORDER BY %s %s LIMIT %d OFFSET %d",
-			sortType, orderBy, pageSize, offset), folderQuery, deckQuery).
-		Scan(&results).Error
+	err := db.Table("(?) AS combined", combinedQuery).
+		Select("combined.id, combined.name, combined.type, COUNT(favorite_storage.storage_id) AS rating, user.profile_icon_path").
+		Joins("LEFT JOIN favorite_storage ON favorite_storage.storage_id = combined.id AND favorite_storage.storage_type = combined.type").
+		Joins("LEFT JOIN permission ON permission.storage_id = combined.id AND permission.storage_type = combined.type").
+		Joins("LEFT JOIN user ON permission.user_id = user.id_user").
+		Where("combined.name LIKE ? AND combined.access_type = ?", fmt.Sprintf("%%%s%%", name), AccessTypePublic).
+		Group("combined.id").
+		Order(fmt.Sprintf("%s %s", sortType, orderBy)).
+		Scopes(Paginate(page, pageSize)).
+		Scan(&results).
+		Error
 	if err != nil {
 		return &[]SearchResult{}, err
 	}
